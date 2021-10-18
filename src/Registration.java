c7c71e462549a05d89173c3c1c21944df7ee7adb
yousufislam <yousufislam337@gmail.com>
2021-10-19 03:55:43 +0600

    completed regex validation of all input fields

--- src/Registration.java
+++ src/Registration.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
 import javax.swing.border.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.util.regex.*;
 
 public class Registration extends JFrame {
@@ -11,40 +13,42 @@ public class Registration extends JFrame {
     private JTextField inputFirstName, inputLastName, inputContactNumber, inputEmail, inputDateOfBirth, inputParmanentAddress, inputCurrentAddress, inputBloodGroup;
     private JButton addImageBtn, signinBtn, registerBtn;
     private Cursor cursor = new Cursor(Cursor.HAND_CURSOR);
-    private ImageIcon icon, addImage, logo;
+    private ImageIcon icon, addImage, logo, chooseImage;
     private JCheckBox checkBox;
     private JPasswordField inputPassword, inputConfirmPassword;
     private JRadioButton male, female;
-    
+    private JFileChooser fileChooser;
+    private String imagePath;
+    private int flag = 0;
+
     private JLabel[] label = new JLabel[11];
     private JTextField[] textFields = new JTextField[8];
     private JPasswordField[] passFields = new JPasswordField[2];
-    
+
     private Font headingfont = new Font("Courier", Font.BOLD, 35);
     private Font txtfont = new Font("Arial", Font.BOLD, 13);
     private Font photofont = new Font("Arial", Font.BOLD, 15);
 
-
     public Registration() {
-        
+
         setSize(900, 700);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        // setResizable(false);
+        setResizable(false);
         setLayout(null);
 
-        //icon set
+        // icon set
         icon = new ImageIcon(getClass().getResource(".//image//icon.jpg"));
         setIconImage(icon.getImage());
-        
-        //body panel add
+
+        // body panel add
         body = new JPanel();
         body.setBounds(0, 0, 900, 700);
         body.setBackground(new Color(0, 32, 63));
         body.setLayout(null);
         add(body);
 
-        //logo add
+        // logo add
         logo = new ImageIcon(getClass().getResource(".//image//R_logo.png"));
         logolabel = new JLabel(logo);
         logolabel.setBounds(30, 10, logo.getIconWidth(), logo.getIconHeight());
@@ -66,7 +70,7 @@ public class Registration extends JFrame {
         addImgLabel.setBounds(80, 130, addImage.getIconWidth(), addImage.getIconHeight());
         body.add(addImgLabel);
 
-        addImageBtn = new JButton("Select photo");
+        addImageBtn = new JButton("Chooses File");
         addImageBtn.setBounds(280, 110, 110, 30);
         addImageBtn.setBackground(new Color(173, 239, 209));
         addImageBtn.setForeground(Color.BLACK);
@@ -80,7 +84,7 @@ public class Registration extends JFrame {
         photoFormate.setBounds(280, 145, 190, 15);
         body.add(photoFormate);
 
-        //naming label field
+        // naming label field
         firstName = new JLabel("First Name : ");
         lastName = new JLabel("Last Name : ");
         contactNumber = new JLabel("Contact Number : ");
@@ -92,7 +96,7 @@ public class Registration extends JFrame {
         gender = new JLabel("Gender: ");
         password = new JLabel("Password: ");
         confirmPassword = new JLabel("Confirm Password: ");
- 
+
         firstName.setBounds(550, 90, 100, 15);
         lastName.setBounds(550, 160, 100, 15);
         contactNumber.setBounds(550, 230, 140, 15);
@@ -104,7 +108,7 @@ public class Registration extends JFrame {
         confirmPassword.setBounds(400, 400, 180, 15);
         parmanentAddress.setBounds(50, 470, 140, 15);
         currentAddress.setBounds(50, 540, 140, 15);
- 
+
         label[0] = firstName;
         label[1] = lastName;
         label[2] = contactNumber;
@@ -116,8 +120,8 @@ public class Registration extends JFrame {
         label[8] = password;
         label[9] = confirmPassword;
         label[10] = gender;
- 
-        for(int i=0; i<11; i++) {
+
+        for (int i = 0; i < 11; i++) {
             label[i].setFont(txtfont);
             label[i].setForeground(Color.WHITE);
             body.add(label[i]);
@@ -151,14 +155,14 @@ public class Registration extends JFrame {
         textFields[6] = inputParmanentAddress;
         textFields[7] = inputCurrentAddress;
 
-        for(int i=0; i<8; i++) {
+        for (int i = 0; i < 8; i++) {
             textFields[i].setFont(txtfont);
             textFields[i].setBackground(Color.WHITE);
             textFields[i].setForeground(Color.BLACK);
             textFields[i].setBorder(null);
             body.add(textFields[i]);
         }
-        
+
         // //add pass field
         inputPassword = new JPasswordField();
         inputConfirmPassword = new JPasswordField();
@@ -169,7 +173,7 @@ public class Registration extends JFrame {
         passFields[0] = inputPassword;
         passFields[1] = inputConfirmPassword;
 
-        for(int i=0; i<2; i++) {
+        for (int i = 0; i < 2; i++) {
             passFields[i].setFont(txtfont);
             passFields[i].setBackground(Color.WHITE);
             passFields[i].setForeground(Color.BLACK);
@@ -177,7 +181,7 @@ public class Registration extends JFrame {
             body.add(passFields[i]);
         }
 
-        //checkbox add
+        // checkbox add
         checkBox = new JCheckBox("Show Password");
         checkBox.setBounds(710, 425, 120, 15);
         checkBox.setBackground(null);
@@ -185,7 +189,7 @@ public class Registration extends JFrame {
         checkBox.setBorder(null);
         body.add(checkBox);
 
-        //button add
+        // button add
         registerBtn = new JButton("Register");
         registerBtn.setBounds(700, 510, 100, 35);
         registerBtn.setBackground(new Color(173, 239, 209));
@@ -204,7 +208,7 @@ public class Registration extends JFrame {
         signinBtn.setFont(txtfont);
         body.add(signinBtn);
 
-        //naming label add
+        // naming label add
         dateFormate = new JLabel("Use formate (dd.mm.yyyy)");
         dateFormate.setForeground(Color.WHITE);
         dateFormate.setBounds(280, 285, 160, 15);
@@ -236,49 +240,165 @@ public class Registration extends JFrame {
         setVisible(true);
 
         checkBox.addActionListener(new ActionListener() {
-            
+
             @Override
             public void actionPerformed(ActionEvent e) {
 
-               if(checkBox.isSelected()) {
-                    inputPassword.setEchoChar((char)0);
-                    inputConfirmPassword.setEchoChar((char)0);
-               }
-               else {
+                if (checkBox.isSelected()) {
+                    inputPassword.setEchoChar((char) 0);
+                    inputConfirmPassword.setEchoChar((char) 0);
+                } else {
                     inputPassword.setEchoChar('\u25CF');
                     inputConfirmPassword.setEchoChar('\u25CF');
-               }
+                }
             }
         });
 
         male.addActionListener(new ActionListener() {
-            
+
             @Override
             public void actionPerformed(ActionEvent e) {
 
-               if(male.isSelected()) {
+                if (male.isSelected()) {
                     female.setSelected(false);
-               }
+                }
             }
         });
 
         female.addActionListener(new ActionListener() {
-            
+
             @Override
             public void actionPerformed(ActionEvent e) {
 
-               if(female.isSelected()) {
+                if (female.isSelected()) {
                     male.setSelected(false);
-               }
+                }
+            }
+        });
+
+        addImageBtn.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if (e.getSource() == addImageBtn) {
+                    try {
+                        fileChooser = new JFileChooser();
+                        fileChooser.setAcceptAllFileFilterUsed(false);
+                        FileNameExtensionFilter filter = new FileNameExtensionFilter("Images", "jpg", "png", "jpeg");
+                        fileChooser.addChoosableFileFilter(filter);
+
+                        int result = fileChooser.showDialog(null, "Open");
+                        if (result == JFileChooser.APPROVE_OPTION) {
+                            File file = fileChooser.getSelectedFile();
+                            imagePath = file.getAbsolutePath();
+                            chooseImage = new ImageIcon(new ImageIcon(imagePath).getImage().getScaledInstance(200, 200,
+                                    Image.SCALE_DEFAULT));
+                            addImageLabel.setIcon(chooseImage);
+                            flag = 1;
+                        } else if (result == JFileChooser.CANCEL_OPTION) {
+                            JOptionPane.showMessageDialog(null, "No file selected");
+                        }
+                    } catch (Exception e2) {
+                        System.err.println("image error: " + e2);
+                    }
+                }
             }
         });
 
         signinBtn.addActionListener(new ActionListener() {
-            
+
             @Override
             public void actionPerformed(ActionEvent e) {
                 dispose();
                 new signin();
+            }
+        });
+
+        registerBtn.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                // get input text
+                String ufname = inputFirstName.getText();
+                String ulname = inputLastName.getText();
+                String udob = inputDateOfBirth.getText();
+                String ucontactnumber = inputContactNumber.getText();
+                String ubloodgroup = inputBloodGroup.getText();
+                String umale = male.getText(); // radio
+                String ufemale = female.getText(); // radio
+                String uemail = inputEmail.getText();
+                String upass = inputPassword.getText();
+                String ucpass = inputConfirmPassword.getText();
+                String upaddress = inputParmanentAddress.getText();
+                String ucaddress = inputCurrentAddress.getText();
+                // File uimage = addImageLabel.getImage();
+
+                // validation input field
+                String nameRegex = "^[a-zA-Z. ]+$";
+                String dobRegex = "((0[1-9])|([1-2][0-9])|(3[0-1])).((0[1-9])|(1[0-2])).((19[0-9][0-9])|(20[0-1][0-9])|(202[0-1]))$";
+                String mobileRegex = "(\\+88)?01[3-9]\\d{8}";
+                String bloodRegex = "((AB\\+)|(AB\\-)|(A\\+)|(A\\-)|(B\\+)|(B\\-)|(O\\+)|(O\\-))$";
+                String emailRegex = "[a-z0-9.]+@[a-z]+[.[a-z]]+$";
+                String passRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{10,}$";
+                String addressRegex = "[A-Za-z0-9'\\.\\-\\s\\,\\/]+$";
+
+                // condition
+                if (!Pattern.matches(nameRegex, ufname)) {
+                    JOptionPane.showMessageDialog(null, "In-valid First Name");
+                }
+
+                else if (!Pattern.matches(nameRegex, ulname)) {
+                    JOptionPane.showMessageDialog(null, "In-valid Last Name");
+                }
+
+                else if (!Pattern.matches(dobRegex, udob)) {
+                    JOptionPane.showMessageDialog(null,"In-valid Date of Birth. \r\n" 
+                        + "You must be follow this format (dd.mm.yyyy)");
+                }
+
+                else if (!Pattern.matches(mobileRegex, ucontactnumber)) {
+                    JOptionPane.showMessageDialog(null, "In-valid Phone Number");
+                }
+
+                else if (!Pattern.matches(bloodRegex, ubloodgroup)) {
+                    JOptionPane.showMessageDialog(null,"In-valid Blood Group. \r\n" 
+                        + "You must write following this format (AB+ / O- / A+)");
+                }
+
+                else if (!((male.isSelected()) || (female.isSelected()))) {
+                    JOptionPane.showMessageDialog(null, "Select your gender");
+                }
+
+                else if (!Pattern.matches(emailRegex, uemail)) {
+                    JOptionPane.showMessageDialog(null, "In-valid E-mail");
+                }
+
+                else if (flag == 0) {
+                    JOptionPane.showMessageDialog(null, "Upload your picture");
+                }
+
+                else if (!upass.equals(ucpass)) {
+                    JOptionPane.showConfirmDialog(null, "Password & confirm password is not matching");
+                }
+
+                else if (!Pattern.matches(passRegex, upass)) {
+                    JOptionPane.showMessageDialog(null, "Your password is not secure. Please set a strong password \r\n"
+                            + "with 10 digits using uppercase, lowercase and special character");
+                }
+
+                else if (!Pattern.matches(addressRegex, upaddress)) {
+                    JOptionPane.showMessageDialog(null, "In-valid parmanent address");
+                }
+
+                else if (!Pattern.matches(addressRegex, ucaddress)) {
+                    JOptionPane.showMessageDialog(null, "In-valid current address");
+                }
+
+                else {
+                    JOptionPane.showMessageDialog(null, "Registration Successfully");
+                }
             }
         });
     }
